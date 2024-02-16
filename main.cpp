e8d745928687d9d538e49a521edc2285843c5e22
snowykte0426 <snowykte0426@naver.com>
2024-02-16 17:02:20 +0900

    함수 수정 및 코드 재정렬

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include <windows.h>
 #include <stdbool.h>
-unsigned short a1 = 0, gal = 0;
-FILE* prrecord = fopen("calculation_record.txt", "w");
+unsigned short a1, gal = 0;
+FILE* prrecord = fopen("calculation_record.txt", "a");
 static short a1null()
 {
+	gal = NULL;
 	a1 = NULL;
-	return a1;
+	return a1, gal;
 }
 static int quitpro()
 {
@@ -44,6 +45,7 @@ int main()
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLUE);
 		printf("사칙연산 계산기 입니다\n");
 		printf("정수연산은 '1'을, 실수연산은 '2'를,나머지연산은 '3'을 입력해주세요\n(나머지 연산은 정수만 지원합니다)\n연산을 할 때 입력 해야만 하는 숫자는 총 4개입니다");
+		FILE* prrecord = fopen("calculation_record.txt", "r");
 		printf("\n계산기록은 10개 까지만 저장됩니다\n------------------\n");
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
 		Sleep(100);
@@ -53,13 +55,13 @@ int main()
 		long double f1 = NULL, f2 = NULL, f3 = NULL, f4 = NULL, finf = NULL;
 		while (intandflart == 1)
 		{
-
-			printf("연산을 할 숫자를 입력해주세요\n4개\nex)10 20 0 0\n");
+            printf("연산을 할 숫자를 입력해주세요\n4개\nex)10 20 0 0\n");
 			scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
 			printf("연산자를 입력해주세요\n(+ - * /)\n");
 		    short a1null();
 		    atreturn:
 			scanf("%c", &a1);
+			Sleep(100);
 			if (a1 == '+') {
 				finn = n1 + n2 + n3 + n4;
 				printf("계산결과는 '%d'입니다", finn);
@@ -68,16 +70,17 @@ int main()
 				printf("\n계속하시겠습니까?(Y/N)\n");
 				scanf("%c", &brk);
 				if (brk == 'Y' || brk == 'y')
-				gal = a1null();
+				a1null();
 				goto reprograms;
 				if (brk == 'N' || brk == 'n') {
-					Sleep(1000);
-					printf("프로그램을 종료합니다\n");
-					gal = quitpro();
+				Sleep(1000);
+				printf("프로그램을 종료합니다\n");
+				break;
+				quitpro();
 				}
 				else {
-					printf("잘못된 값을 입력하셨습니다\n'N'을 입력한걸로 간주하고 프로그램을 종료합니다\n");
-					exit(0);
+				printf("잘못된 값을 입력하셨습니다\n'N'을 입력한걸로 간주하고 프로그램을 종료합니다\n");
+				exit(0);
 				}
 			}
 			if (a1 == '-') {
